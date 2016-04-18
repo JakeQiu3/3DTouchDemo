52238efffda49ed938164208c8029d28480e3e7e
qsyMac <qsy118614@126.com>
2016-04-19 00:30:19 +0800

    新增RN学习链接

--- 3DTouchDemo/AppDelegate.h
+++ 3DTouchDemo/AppDelegate.h
@@ -4,8 +4,10 @@
 //
 //  Created by 邱少依 on 16/4/8.
 //  Copyright © 2016年 QSY. All rights reserved.
-//
-
+//React Native 开发
+//http://www.linuxidc.com/Linux/2015-09/123239.htm
+//http://www.tuicool.com/articles/zaInUbA
+//http://reactnative.cn/docs/0.23/getting-started.html#content
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
